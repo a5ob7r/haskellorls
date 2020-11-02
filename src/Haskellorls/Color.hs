0392416314e610d7e326022d15d510d8e09f4ae5
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2020-11-02 20:33:03 +0900

    Refactor Haskellorls.Color module

--- src/Haskellorls/Color.hs
+++ src/Haskellorls/Color.hs
@@ -1,10 +1,11 @@
 module Haskellorls.Color where
 
 import Data.Char (toUpper)
-import qualified Data.Map.Strict as Map
+import qualified Data.Map.Strict as Map (Map, lookup, fromList)
+import Data.List (isPrefixOf)
 import Data.List.Extra (tails)
 import Data.List.Split (endBy, splitOn)
-import Data.Maybe
+import Data.Maybe (mapMaybe)
 import System.Environment (getEnv)
 
 type FilenamePtnMap = Map.Map String String
@@ -18,34 +19,28 @@ colorize esc str = "\^[[" ++ esc ++ "m" ++ str ++ "\^[[m"
     fails to lookup, change a query to the extension and re lookup.
 -}
 lookupEscSec :: FilenamePtnMap -> String -> String
-lookupEscSec ptnMap = f . mapMaybe (\q -> Map.lookup q ptnMap) . reverse . tails . toUppers
+lookupEscSec ptnMap = f . mapMaybe (`Map.lookup` ptnMap) . reverse . tails . toUppers
   where f [] = ""
         f xs = head xs
 
 colorIndicators :: IO FilenamePtnMap
 colorIndicators = do
   envLSCOLORS <- getLSCOLORS
-  return . Map.fromList . filenameEscSecs . concat . map (maybeToList . makePatternEscapePair) . endBy ":" $ envLSCOLORS
-
-filenameEscSecs :: [(String, String)] -> [(String, String)]
-filenameEscSecs =  concat . map (maybeToList . filenamePtnEscSec)
+  return . Map.fromList . mapMaybe f . endBy ":" $ envLSCOLORS
+    where f s = makePatternEscapePair s >>= filenamePtnEscSec
 
 filenamePtnEscSec :: (String, String) -> Maybe (String, String)
 filenamePtnEscSec (ptn, esc) = filenamePattern ptn >>= (\ext -> Just (ext, esc))
 
 filenamePattern :: String -> Maybe String
-filenamePattern str = f str >>= g
-  where f s = if (length s) >= 1
-                 then Just s
-                 else Nothing
-        g s = if (head s) == '*'
-                 then Just . toUppers . drop 1 $ s
-                 else Nothing
+filenamePattern str = if "*" `isPrefixOf` str
+                         then Just . toUppers . drop 1 $ str
+                         else Nothing
 
 makePatternEscapePair :: String -> Maybe (String, String)
-makePatternEscapePair s = if (length pairs) == 2
-                              then Just (head pairs, last pairs)
-                              else Nothing
+makePatternEscapePair s = if length pairs == 2
+                             then Just (head pairs, last pairs)
+                             else Nothing
   where pairs = splitOn "=" s
 
 getLSCOLORS :: IO String
