1a9b06c139f78fee6cf3d57c6652558740a51caf
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2020-11-20 14:52:45 +0900

    Add grid format

--- haskellorls.hs
+++ haskellorls.hs
@@ -1,13 +1,13 @@
 module Main where
 
 import Data.Maybe (fromMaybe, listToMaybe)
-
 import System.Directory.Extra
 import System.IO
 import qualified Options.Applicative as OA
 
 import qualified Haskellorls.Color as Color
 import Haskellorls.Decorator
+import qualified Haskellorls.Grid as Grid
 import Haskellorls.Node
 import qualified Haskellorls.Option as Option
 
@@ -27,8 +27,13 @@ run opt = do
                       Option.ALWAYS -> return True
                       Option.AUTO -> hIsTerminalDevice stdout
   let additionals = decorator nodes fs
-      nodeNamesForDisplay = if shouldColorize
-                               then map (Color.colorizedNodeName cConfig) nodes
-                               else map nodeName nodes
-  mapM_ (putStrLn . (\(a, b) -> a ++ " " ++ b)) $ zip additionals nodeNamesForDisplay
+      nodePrinter = if shouldColorize
+                       then Color.colorizedNodeName cConfig
+                       else nodeName
+      nodeNamesForDisplay = map nodePrinter nodes
+  if Option.long opt
+     then do
+       mapM_ (putStrLn . (\(a, b) -> a ++ " " ++ b)) $ zip additionals nodeNamesForDisplay
+     else do
+       Grid.showNodesWithGridForm nodes nodePrinter
     where fs = [nodeMode, nodeOwner, nodeGroup, nodeSize, nodeMtime]
---
+++ src/Haskellorls/Grid.hs
@@ -0,0 +1,71 @@
+module Haskellorls.Grid
+  ( showNodesWithGridForm
+  ) where
+
+import Data.List (intercalate, transpose)
+import qualified System.Console.Terminal.Size as TS
+
+import Haskellorls.Node
+
+newtype Grid = Grid { unGrid :: [[String]] }
+  deriving Show
+
+showNodesWithGridForm :: [Node] -> (Node -> String) -> IO ()
+showNodesWithGridForm nodes nodePrinter = do
+  colSize <- terminalColumnSize
+  let names = map nodeName nodes
+      grid = validGrid names colSize
+      maxColLens = maximumColumnLengths grid
+      nodeGrid = transNest (length $ unGrid grid) nodes
+      gridLines = map (\nds -> gridLine nds maxColLens nodePrinter) nodeGrid
+  mapM_ putStrLn gridLines
+
+gridMargin :: String
+gridMargin = "  "
+
+validateGrid :: Int -> Grid -> Bool
+validateGrid n grid
+  | n == 0 = False
+  | otherwise = n >= sum maxColLens + paddings
+    where
+      maxColLens = maximumColumnLengths grid
+      paddings = length gridMargin * relu (length maxColLens - 1)
+
+relu :: Int -> Int
+relu n
+  | n >= 0 = n
+  | otherwise = -n
+
+gridLine :: [Node] -> [Int] -> (Node -> String) -> String
+gridLine ns maxColLens nodePrinter = intercalate gridMargin $ zipWith (\nd pad -> nodePrinter nd ++ replicate pad ' ') ns paddings
+  where
+    paddings = zipWith (\nd colLen -> colLen - length (nodeName nd)) ns maxColLens
+
+validGrid :: [String] -> Int -> Grid
+validGrid ss colSize = validGrid' ss colSize 1
+
+validGrid' :: [String] -> Int -> Int -> Grid
+validGrid' ss colSize colNum
+  | ssNum <= colNum = grid
+  | validateGrid colSize grid = grid
+  | otherwise = validGrid' ss colSize $ colNum + 1
+    where
+      grid = makeGrid colNum ss
+      ssNum = length ss
+
+maximumColumnLengths :: Grid -> [Int]
+maximumColumnLengths (Grid grid) = map (maximum . map length) . transpose $ grid
+
+terminalColumnSize :: IO Int
+terminalColumnSize = maybe 0 TS.width <$> TS.size
+
+makeGrid :: Int -> [String] -> Grid
+makeGrid n = Grid . transNest n
+
+transNest :: Int -> [a] -> [[a]]
+transNest n = transpose . nest n
+
+nest :: Int -> [a] -> [[a]]
+nest n xs = if length xs > n
+               then take n xs:nest n (drop n xs)
+               else [xs]
