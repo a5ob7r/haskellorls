a5ob7r/haskellorls

commit 5eabaa98445597910d5064dd65ec73d16089f4ea

Remove 'snoc'

src/Haskellorls/Tree/Decorator.hs
@@ -5,6 +5,8 @@ module Haskellorls.Tree.Decorator
 where
 
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.Builder as L
 import qualified Haskellorls.LsColor as Color
 import Haskellorls.Tree.Type
 import qualified Haskellorls.WrappedText as WT
@@ -12,7 +14,7 @@ import qualified Haskellorls.WrappedText as WT
 treeBranch :: [TreeNodePosition] -> T.Text
 treeBranch [] = ""
 treeBranch [x] = toTextOnLast x
-treeBranch (x : xs) = toTextOnInit x <> treeBranch xs
+treeBranch (x : xs) = L.toStrict . L.toLazyText $ foldr (\a acc -> L.fromText (toTextOnInit a) <> acc) (L.fromText $ toTextOnLast x) xs
 
 treeBranchWithColor :: Color.LsColors -> [TreeNodePosition] -> [WT.WrappedText]
 treeBranchWithColor _ [] = []

src/Haskellorls/NodeInfo.hs
@@ -152,6 +152,7 @@ data NodeInfo = NodeInfo
     getNodeContext :: T.Text,
     getNodeDirName :: RawFilePath,
     getNodeLinkInfo :: Maybe (Either OrphanedLinkNodeInfo LinkNodeInfo),
+    -- | This contains reversed list due to the performance problem.
     getTreeNodePositions :: [Tree.TreeNodePosition]
   }
 

src/Haskellorls/Tree/Util.hs
@@ -6,7 +6,6 @@ where
 
 import Control.Monad.IO.Class
 import Control.Monad.State.Strict
-import qualified Data.List.Extra as L
 import qualified Data.Sequence as S
 import qualified Haskellorls.Depth as Depth
 import Haskellorls.Exception
@@ -21,15 +20,15 @@ import System.FilePath.Posix.ByteString
 makeSomeNewPositionsList :: Int -> [TreeNodePosition] -> [[TreeNodePosition]]
 makeSomeNewPositionsList n _
   | n < 1 = [[]]
-makeSomeNewPositionsList 1 xs = [L.snoc xs LAST]
+makeSomeNewPositionsList 1 xs = [LAST : xs]
 makeSomeNewPositionsList n xs = case replicate n xs of
   [] -> []
-  (y : ys) -> L.snoc y HEAD : makeSomeNewPositionsList' ys
+  (y : ys) -> (HEAD : y) : makeSomeNewPositionsList' ys
 
 makeSomeNewPositionsList' :: [[TreeNodePosition]] -> [[TreeNodePosition]]
 makeSomeNewPositionsList' [] = []
-makeSomeNewPositionsList' [x] = [L.snoc x LAST]
-makeSomeNewPositionsList' (x : xs) = L.snoc x MID : makeSomeNewPositionsList' xs
+makeSomeNewPositionsList' [x] = [LAST : x]
+makeSomeNewPositionsList' (x : xs) = (MID : x) : makeSomeNewPositionsList' xs
 
 makeTreeNodeInfos :: (MonadCatch m, MonadIO m) => Option.Option -> RawFilePath -> m (S.Seq Node.NodeInfo)
 makeTreeNodeInfos opt path = do

commit 6836240c56aa5741e863cfbc39f1376267298294

Fix --tree
The --tree option's rendering is broken.

```
files/examples08
├── 1
│  └── 2
   │  └── 3
      │  └── 4
         │  └── 5
            │  └── 6
└── 7
   └── 8
      └── 9
```

This regression is introduced by 5eabaa9.

src/Haskellorls/Tree/Decorator.hs
@@ -4,6 +4,7 @@ module Haskellorls.Tree.Decorator
   )
 where
 
+import Data.List (foldl')
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.Builder as L
@@ -14,7 +15,7 @@ import qualified Haskellorls.WrappedText as WT
 treeBranch :: [TreeNodePosition] -> T.Text
 treeBranch [] = ""
 treeBranch [x] = toTextOnLast x
-treeBranch (x : xs) = L.toStrict . L.toLazyText $ foldr (\a acc -> L.fromText (toTextOnInit a) <> acc) (L.fromText $ toTextOnLast x) xs
+treeBranch (x : xs) = L.toStrict . L.toLazyText $ foldl' (\acc a -> L.fromText (toTextOnInit a) <> acc) (L.fromText $ toTextOnLast x) xs
 
 treeBranchWithColor :: Color.LsColors -> [TreeNodePosition] -> [WT.WrappedText]
 treeBranchWithColor _ [] = []

golden-test/TreeDeep.goldplate
@@ -1,0 +1,8 @@
+{
+  "command": "haskellorls",
+  "arguments": ["--tree", "files/examples08"],
+  "asserts": [
+    {"exit_code": 0},
+    {"stdout": "${GOLDPLATE_NAME}.stdout"}
+  ]
+}

golden-test/TreeDeep.stdout
@@ -1,0 +1,10 @@
+files/examples08
+├── 1
+│  └── 2
+│     └── 3
+│        └── 4
+│           └── 5
+│              └── 6
+└── 7
+   └── 8
+      └── 9